f8c09e8d10327252fc26ab1b16e4f61317738f8c
George Young <george@georgey.co.uk>
2013-10-13 14:03:39 +0100

    Test of remove

---
+++ src/uk/co/georgey/trial/DoNothing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package uk.co.georgey.trial;
+
+/**
+ * @author George Young
+ *
+ */
+public class DoNothing {
+
+	public void doNout(){
+		//this does nothing
+		System.out.println("This does nowt");
+		
+	}
+	
+	
+}
